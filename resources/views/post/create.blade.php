7fc91ce789adfa06d75393cb5153897e11f36c0e
s1llhoutte <danikval2000002@gmail.com>
2024-07-02 14:59:32 +0300

    modernisation crud pr.4

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,21 +5,32 @@
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" name="title" class="form-control" id="title" placeholder="Title">
+                <input value="{{ old('title') }}"  type="text" name="title" class="form-control" id="title" placeholder="Title">
+                @error('title')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="content" class="form-label">Content</label>
-                <textarea name="content" class="form-control" id="content" placeholder="Content"></textarea>
+                <textarea name="content" class="form-control" id="content" placeholder="Content">{{ old('content') }}</textarea>
+                @error('content')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Image</label>
-                <input type="text" name="image" class="form-control" id="image" placeholder="Image">
+                <input value="{{ old('image') }}" type="text" name="image" class="form-control" id="image" placeholder="Image">
+                @error('image')
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
             </div>
             <div class="form-group mb-3">
-                <label for="Category">Category</label>
-                <select class="form-control" id="Category" name="category_id">
+                <label for="category">Category</label>
+                <select class="form-control" id="category" name="category_id">
                     @foreach($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->title }}</option>
+                        <option
+                            {{ old('category_id') == $category->id ? ' selected' : '' }}
+                            value="{{ $category->id }}">{{ $category->title }}</option>
                     @endforeach
                 </select>
             </div>
