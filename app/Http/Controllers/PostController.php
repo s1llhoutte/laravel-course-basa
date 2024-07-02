17748eaab9e57a5ed65f97c660a07b7e8b8f5807
s1llhoutte <danikval2000002@gmail.com>
2024-07-02 12:07:34 +0300

    mnogie ko mnogim

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,12 +12,11 @@ class PostController extends Controller
     public function index()
     {
 //        $posts = Post::all();
-        $category = Category::find(1);
+
         $post = Post::find(1);
+        $tag = Tag::find(1);
 
-
-
-        dd($post->category);
+        dd($tag->posts);
 
         // return view('post.index', compact('posts'));
     }
