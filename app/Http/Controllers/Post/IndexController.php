7bb7103ddb676c21283d229e9906cd6747d153f5
s1llhoutte <danikval2000002@gmail.com>
2024-07-05 14:26:16 +0300

    filter laravel

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\PostFilter;
+use App\Http\Requests\Post\FilterRequest;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\PostTag;
@@ -11,9 +13,13 @@ use Illuminate\Http\Request;
 
 class IndexController extends BaseController
 {
-    public function __invoke()
+    public function __invoke(FilterRequest $request)
     {
-        $posts = Post::paginate(10);
+        $data = $request->validated();
+
+        $filter = app()->make(PostFilter::class, ['queryParams' => array_filter($data)]);
+        $posts = Post::filter($filter)->paginate(10);
+
         return view('post.index', compact('posts'));
     }
 }
