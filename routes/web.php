e5f6d566150c86e35c0b3fd522059900e62f52de
s1llhoutte <danikval2000002@gmail.com>
2024-07-09 13:32:25 +0300

    auth account

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'aaa';
-});
+Route::get('/', 'App\Http\Controllers\HomeController@index');
 
 Route::group(['namespace'=>'App\Http\Controllers\Post'], function(){
     Route::get('/posts', 'IndexController' )->name('post.index');
@@ -30,3 +29,7 @@ Route::get('/posts/update_or_create', 'App\Http\Controllers\PostController@updat
 Route::get('/main', 'App\Http\Controllers\MainController@index')->name('main.index');
 Route::get('/contacts', 'App\Http\Controllers\ContactController@index')->name('contact.index');
 Route::get('/about', 'App\Http\Controllers\AboutController@index')->name('about.index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
